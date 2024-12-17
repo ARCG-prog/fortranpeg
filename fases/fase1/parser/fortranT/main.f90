d36f74a22bb1cca6f7a9939f0b4aba5f369ed012
MasterChief123XD <1usaclex@gmail.com>
2024-12-17 15:11:25 -0600

    f90, como modular el inicio aplicacion
    antes comenzar modular mas o menos la aplicación para poder luego ser echa con el visitor

--- fases/fase1/parser/fortranT/main.f90
+++ fases/fase1/parser/fortranT/main.f90
@@ -1,22 +1,42 @@
+module fortranModule
+    implicit none
+    private
+    public :: principal
+    public :: impChar
+
+    contains
+        subroutine principal(tam, txt)
+            integer, intent(in) :: tam
+            character(len=:), allocatable, target, intent(inout) :: txt
+            integer :: i
+
+            ! Imprimir cada carácter usando el puntero
+            do i = 1, tam
+                call impChar(txt(i:i))
+            end do
+            
+        end subroutine principal
+
+        subroutine impChar(char)
+            character(len=1), intent(in) :: char
+            print *, char
+        end subroutine impChar
+
+end module fortranModule
+
 program main
+    use fortranModule
     implicit none
     character(len=:), allocatable, target :: txt
-    character(len=1), pointer :: char_ptr
-    integer :: tam, i
+    integer :: tam
 
     ! Definir el tamaño de la cadena
     tam = len("Hola mundo")
     allocate(character(len=tam) :: txt)
     txt = "Hola mundo"
 
-    ! Asignar el puntero al primer carácter de la cadena
-    char_ptr => txt(1:1)
-
-    ! Imprimir cada carácter usando el puntero
-    do i = 1, tam
-        print *, char_ptr
-        if (i < tam) char_ptr => txt(i+1:i+1)
-    end do
+    ! Llamar a la subrutina principal con los parámetros
+    call principal(tam, txt)
 
     ! Liberar la memoria
     deallocate(txt)
